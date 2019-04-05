1dde5426d6fbf35171ef81d265f72649304a060e
Unknown <coleipod@gmail.com>
2019-04-05 16:55:54 -0400

    More compatibility!

--- setup/install.sh
+++ setup/install.sh
@@ -6,6 +6,15 @@
 
 echo "Beginning installation"
 #update packages to make sure the install goes properly
+
+echo "Would you like to add the PHP7.2 repository (this is needed on some systems, like Debian) [Y/n]: "
+read resp
+if [[ "$resp" =~ ^([yY][eE][sS]|[yY])+$ ]]
+  sudo apt-get install apt-transport-https lsb-release ca-certificates
+  wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+fi
+
 echo "Updating APT"
 sudo apt-get update > /dev/null
 
@@ -19,7 +28,7 @@ sudo apt-get install mysql-server -y > /dev/null
 #set up MySQL server
 echo "Setting up MySQL server"
 #update password in the PHP 'database.php' file
- cat /var/www/html/database.php | sed s/CHANGEME/$mysql_password/ | sudo tee /var/www/html/database.php
+ cat /var/www/html/database.php | sed s/CHANGEME/$mysql_password/ | sudo tee /var/www/html/database.php > /dev/null
 #proceed with the MySQL secure installation
  printf "$mysql_password\nN\nN\nY\nY\nY\nY\n" | sudo mysql_secure_installation > /dev/null
 
@@ -30,6 +39,7 @@ echo "Creating SocialBook database"
 
 #turn on website services
 echo "Standing up website"
+sudo chown -R www-data:www-data /var/www/html
 sudo /etc/init.d/mysql start
 sudo /etc/init.d/apache2 start
 sudo a2enmod php7.2 > /dev/null
